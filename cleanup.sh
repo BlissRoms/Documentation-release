6cbc77f4624314f2fc4f3a28fef76ab0c08b32f8
Eric Park <ideamaneric@gmail.com>
2019-08-11 01:32:53 +0800

    Add cleanup script
    Change-Id: I4c82837d15c770ece91e6e0355ee7bf583a16d5e

---
+++ cleanup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Removes all build artifacts
+rm 404.html
+rm index.html
+rm sitemap.xml
+rm sitemap.xml.gz
+
+rm -r 'Bliss OS'
+rm -r BlissRoms
+rm -r common
+rm -r infrastructure
+rm -r assets
+rm -r search
+
